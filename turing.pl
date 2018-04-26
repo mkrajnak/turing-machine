84375e0d8df6803e9185e819e77c1c1f0d751d69
Martin Krajnak <mkrajnak@redhat.com>
2018-04-26 23:20:59 +0200

    read file, stdin

--- turing.pl
+++ turing.pl
@@ -11,22 +11,60 @@ first([X|_],Y) :- Y = X.
 empty([]) :- true.
 empty([_|_]) :- false.
 
-getline(end_of_file, _, _) :- !.
-getline('\n',_, _) :- !.
-getline(C, Stream, X) :-
-  write(C),
-  X = [C|X],
-  get_char(Stream, C2),
-  write(C2),
-  getline(C2, Stream, X).
-
 
 main(Argv) :-
-  % first(Argv, X),
-  % writeln(X).
   first(Argv, Y),
-  open(Y, read, Stream),
-  get_char(Stream, C),
-  getline(C, Stream, X),
-  write(X),
-  close(Stream).
+  (Y == [],
+    prompt(_, ''),
+    read_lines(LL),!;
+    open(Y, read, Stream),
+    read_lines(Stream, LL)
+  ),
+  write(LL);
+  split_lines(LL, S),
+  write(S),
+  halt.
+
+% Reads line from stdin, terminates on LF or EOF.
+read_line(L,C) :-
+  get_char(C),
+  (isEOFEOL(C), L = [], !;
+		read_line(LL,_),% atom_codes(C,[Cd]),
+		[C|LL] = L).
+
+read_line(Stream,L,C) :-
+  get_char(Stream,C),
+  (isEOFEOL(C), L = [], !;
+		read_line(Stream,LL,_),% atom_codes(C,[Cd]),
+		[C|LL] = L).
+
+
+% Tests if character is EOF or LF.
+isEOFEOL(C) :-
+	C == end_of_file;
+	(char_code(C,Code), Code==10).
+
+% Read file
+read_lines(Stream,Ls) :-
+	read_line(Stream,L,C),
+	( C == end_of_file, Ls = [] ;
+	  read_lines(Stream,LLs), Ls = [L|LLs]
+	).
+
+% Read lines
+read_lines(Ls) :-
+	read_line(L,C),
+	( C == end_of_file, Ls = [] ;
+	  read_lines(LLs), Ls = [L|LLs]
+	).
+
+
+% rozdeli radek na podseznamy
+split_line([],[[]]) :- !.
+split_line([' '|T], [[]|S1]) :- !, split_line(T,S1).
+split_line([32|T], [[]|S1]) :- !, split_line(T,S1).    % aby to fungovalo i s retezcem na miste seznamu
+split_line([H|T], [[H|G]|S1]) :- split_line(T,[G|S1]). % G je prvni seznam ze seznamu seznamu G|S1
+
+% vstupem je seznam radku (kazdy radek je seznam znaku)
+split_lines([],[]).
+split_lines([L|Ls],[H|T]) :- split_lines(Ls,T), split_line(L,H).
